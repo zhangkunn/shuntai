95166b3a1b15c101f12afe1212799592c95c33d1
zxahgkyh <zxahgkyh@live.com>
2016-09-03 10:54:10 +0800

    zhga
    sfhdfds

---
+++ shuntai-server/src/main/java/com/shuntai/server/weixinMenuServer.java
@@ -0,0 +1,20 @@
+package com.shuntai.server;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by zxahgkyh on 16/9/3.
+ */
+
+@Service
+public class WeixinMenuServer {
+
+
+	@PostConstruct
+	public void initMenu(){
+
+	}
+
+}
